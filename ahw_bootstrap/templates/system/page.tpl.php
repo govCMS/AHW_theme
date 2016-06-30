745bb5cd126543cc72ae3a3971f6c7a3ffd534cd
Stuart Rowlands <stuart.rowlands@acquia.com>
2016-07-01 04:25:34 +1000

    Latest migrated styles.

--- ahw_bootstrap/templates/system/page.tpl.php
+++ ahw_bootstrap/templates/system/page.tpl.php
@@ -76,26 +76,17 @@
 <header id="navbar siteheader" role="banner">
   <div class="headerInner">
     <div class="headerContent container">
-
-     <section class="headerLeft">
-          <?php if ($logo): ?>
+      <section class="headerLeft">
+        <?php if ($logo): ?>
           <a class="logo navbar-btn pull-left" href="http://www.environment.gov.au" title="environment.gov.au">
             <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           </a>
         <?php endif; ?>
-        </section>
-
-        <section class="headerRight">
-          <a href="<?php print $front_page; ?>"><img src="/<?php print drupal_get_path('theme', 'ahw_bootstrap'); ?>/images/HeritageWeek.png" alt="Australian Heratige Week 2012" title="<?php print t('Home'); ?>"></a>
-        </section>
-
-
-
-
-
-
+      </section>
+      <section class="headerRight">
+        <a href="<?php print $front_page; ?>"><img src="<?php print drupal_get_path('theme', 'ahw_bootstrap'); ?>/images/HeritageWeek.png" alt="Australian Heratige Week 2012" title="<?php print t('Home'); ?>"></a>
+      </section>
     </div>
-
     <?php if (!empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
         <nav role="navigation">
@@ -107,64 +98,49 @@
     <?php endif; ?>
   </div>
 </header>
-
-
-
-  <section id="feature">
-
-    <?php print render($page['header']); ?>
-  </section> <!-- /#feature -->
-
-
-
-    <section id="main" role="main" class="container">
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-
-
-
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <aside id="sidebar" class="sidebar" role="complementary">
-          <div class="innerFilter">
-            <?php print render($page['sidebar_first']); ?>
-          </div>
-        </aside>  <!-- /#sidebar-first -->
-      <?php endif; ?>
-
-      <?php print render($page['content']); ?>
-
-      <?php if (!empty($page['sidebar_second'])): ?>
-        <aside id="sidebarHome" role="complementary">
-          <section id="whatsOn">
-            <div class="squiggleWrapper"><h3 class="squiggle"><span>What’s on?</span></h3></div>
-            <?php print render($page['sidebar_second']); ?>
-            <aside class="viewMore squiggleLink">
-              <a href="/whats-on">View more events</a>
-            </aside>
-          </section>
-        </aside><!-- /#sidebar-second -->
-      <?php endif; ?>
-
-    </section>
-
+<section id="feature">
+  <?php print render($page['header']); ?>
+</section> <!-- /#feature -->
+<section id="main" role="main" class="container">
+  <?php if (!empty($page['highlighted'])): ?>
+    <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+  <?php endif; ?>
+  <a id="main-content"></a>
+  <?php print render($title_prefix); ?>
+  <?php if (!empty($title)): ?>
+    <h1 class="page-header"><?php print $title; ?></h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php print $messages; ?>
+  <?php if (!empty($tabs)): ?>
+    <?php print render($tabs); ?>
+  <?php endif; ?>
+  <?php if (!empty($page['help'])): ?>
+    <?php print render($page['help']); ?>
+  <?php endif; ?>
+  <?php if (!empty($action_links)): ?>
+    <ul class="action-links"><?php print render($action_links); ?></ul>
+  <?php endif; ?>
+  <?php if (!empty($page['sidebar_first'])): ?>
+    <aside id="sidebar" class="sidebar" role="complementary">
+      <div class="innerFilter">
+        <?php print render($page['sidebar_first']); ?>
+      </div>
+    </aside>  <!-- /#sidebar-first -->
+  <?php endif; ?>
+  <?php print render($page['content']); ?>
+  <?php if (!empty($page['sidebar_second'])): ?>
+    <aside id="sidebarHome" role="complementary">
+      <section id="whatsOn">
+        <div class="squiggleWrapper"><h3 class="squiggle"><span>What’s on?</span></h3></div>
+        <?php print render($page['sidebar_second']); ?>
+        <aside class="viewMore squiggleLink">
+          <a href="/whats-on">View more events</a>
+        </aside>
+      </section>
+    </aside><!-- /#sidebar-second -->
+  <?php endif; ?>
+</section>
 <?php if (!empty($page['footer'])): ?>
   <footer class="footer">
     <?php print render($page['footer']); ?>
